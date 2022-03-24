082ec52bb41cfabe35a0cf0fe5549badf3e74d71
Nishanth <rajkumarnishanth@gmail.com>
2022-03-24 19:56:48 +0530

    feat: monthly wage to meet wage condition

--- EmployeeWageApp/app_main.py
+++ EmployeeWageApp/app_main.py
@@ -2,11 +2,13 @@
     @Author: Nishanth
     @Date: 24-03-2022 14:52:00
     @Last Modified by: Nishanth
-    @Last Modified time: 24-03-2022 19:27:00
+    @Last Modified time: 24-03-2022 19:54:00
     @Title: Entry point of the project
 '''
 from wage_computation import EmployeeWageComputation
 
 employee1 = EmployeeWageComputation()
 
-print("Monthly Wage: ", employee1.monthly_wage())
+print("Monthly Wage: ", employee1.wage_condition())
+print("Total hours worked: ", employee1.total_hours_worked)
+print("Total days worked: ", employee1.total_days_worked)
--- EmployeeWageApp/wage_computation.py
+++ EmployeeWageApp/wage_computation.py
@@ -2,12 +2,25 @@
     @Author: Nishanth
     @Date: 24-03-2022 14:52:00
     @Last Modified by: Nishanth
-    @Last Modified time: 24-03-2022 19:27:00
+    @Last Modified time: 24-03-2022 19:54:00
     @Title: To compute employee wage as given in the use cases
 '''
+from cgitb import reset
 import random
 
 class EmployeeWageComputation:
+
+    def __init__(self):
+        self.total_days_worked = 0
+        self.total_hours_worked = 0
+    
+    def reset(self):
+        """
+            Description:
+                Resets the total days and hours worked to 0
+        """
+        self.total_days_worked = 0
+        self.total_hours_worked = 0
 
     def attendance(self):
         """
@@ -26,7 +39,7 @@ class EmployeeWageComputation:
                 Total hours depends on if employee is part-time or full-time.
                 Part-time/Full-time is decided using random module
             Return:
-                returns the daily wage
+                returns the daily wage and hours worked that day
         """
         wage_per_hour = 20
         total_hours_case = {
@@ -34,7 +47,7 @@ class EmployeeWageComputation:
             False: 4
         }
         total_hours = total_hours_case[random.choice([True, False])]
-        return total_hours*wage_per_hour
+        return (total_hours*wage_per_hour, total_hours)
     
     def monthly_wage(self):
         """
@@ -46,5 +59,23 @@ class EmployeeWageComputation:
         total_wage = 0
         for _ in range(0,20):
             if self.attendance():
-                total_wage += self.daily_wage()
+                daily_wage, hours_worked = self.daily_wage()
+                total_wage += daily_wage
+                self.total_days_worked += 1
+                self.total_hours_worked += hours_worked
         return total_wage
+    
+    def wage_condition(self):
+        """
+            Description:
+                Repeatedly computes monthly wage until 
+                either total hours worked is 100 or total days worked is 20
+            Return:
+                returns monthly wage that meets the condition
+        """
+        total_wage = 0
+        while (self.total_days_worked < 20 and self.total_hours_worked < 100):
+            self.reset()
+            total_wage = self.monthly_wage()
+        else:
+            return total_wage
