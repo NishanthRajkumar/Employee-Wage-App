61f4a071194939ddac5412ef4ad6a79f3ceddfa6
Nishanth <rajkumarnishanth@gmail.com>
2022-03-24 19:31:05 +0530

    feat: compute monthly wage
    Assume no of working days in a month is 20

--- EmployeeWageApp/app_main.py
+++ EmployeeWageApp/app_main.py
@@ -1,8 +1,12 @@
+'''
+    @Author: Nishanth
+    @Date: 24-03-2022 14:52:00
+    @Last Modified by: Nishanth
+    @Last Modified time: 24-03-2022 19:27:00
+    @Title: Entry point of the project
+'''
 from wage_computation import EmployeeWageComputation
 
-if EmployeeWageComputation.attendance():
-    print("Employee is present")
-else:
-    print("Employee is absent")
+employee1 = EmployeeWageComputation()
 
-print("Daily Wage: ", EmployeeWageComputation.daily_wage())
+print("Monthly Wage: ", employee1.monthly_wage())
--- EmployeeWageApp/wage_computation.py
+++ EmployeeWageApp/wage_computation.py
@@ -1,13 +1,15 @@
 '''
     @Author: Nishanth
     @Date: 24-03-2022 14:52:00
+    @Last Modified by: Nishanth
+    @Last Modified time: 24-03-2022 19:27:00
     @Title: To compute employee wage as given in the use cases
 '''
 import random
 
 class EmployeeWageComputation:
 
-    def attendance():
+    def attendance(self):
         """
             Description:
                 checks if employee is absent or present.
@@ -17,7 +19,7 @@ class EmployeeWageComputation:
         """
         return random.choice([True, False])
     
-    def daily_wage():
+    def daily_wage(self):
         """
             Description:
                 Computes daily wage for total hours worked.
@@ -33,3 +35,16 @@ class EmployeeWageComputation:
         }
         total_hours = total_hours_case[random.choice([True, False])]
         return total_hours*wage_per_hour
+    
+    def monthly_wage(self):
+        """
+            Description:
+                Computes monthly wage assuming no of working days per month as 20.
+            Return:
+                returns the monthly wage
+        """
+        total_wage = 0
+        for _ in range(0,20):
+            if self.attendance():
+                total_wage += self.daily_wage()
+        return total_wage
