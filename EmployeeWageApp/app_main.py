9286962b91d2902a92b4724f4ec18372259182d5
Nishanth <rajkumarnishanth@gmail.com>
2022-03-25 17:14:13 +0530

    refactor: logging to console and file

--- EmployeeWageApp/app_main.py
+++ EmployeeWageApp/app_main.py
@@ -5,13 +5,18 @@
     @Last Modified time: 24-03-2022 20:13:00
     @Title: Entry point of the project
 '''
+import sys
 from wage_computation import EmployeeWageComputation
+import logging
+
+file_path = "C:\\Users\\Nishanth\\Desktop\\codingclub\\CFP\\Repos\\Employee-Wage-App\\EmployeeWageApp\\Log files\\Wage.log"
+
+logging.basicConfig(handlers=[logging.FileHandler(file_path, mode="a"), logging.StreamHandler(sys.stdout)], level=logging.INFO)
 
 employee1 = EmployeeWageComputation()
 
-print("Monthly Wage: ", employee1.wage_condition())
-print("Total hours worked: ", employee1.get_work_hours())
-print("Total days worked: ", employee1.total_days_worked)
-print("Wage Log: ")
+logging.info(f"Monthly Wage: {employee1.wage_condition()}")
+logging.info(f"Total hours worked: {employee1.get_work_hours()}")
+logging.info(f"Total days worked: {employee1.total_days_worked}")
 for wage in employee1.wage_log:
-    print(wage)
+    logging.info(f"Wage info(total_wage, (day, daily_wage)): {wage}")
