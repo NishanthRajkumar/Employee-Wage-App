cc4d9a7d824ec14054c067c61cdba58c081bd362
Nishanth <rajkumarnishanth@gmail.com>
2022-03-25 10:44:05 +0530

    refactor: relate attendance to daily_wage

--- EmployeeWageApp/app_main.py
+++ EmployeeWageApp/app_main.py
@@ -2,7 +2,6 @@ from wage_computation import EmployeeWageComputation
 
 if EmployeeWageComputation.attendance():
     print("Employee is present")
+    print("Daily Wage: ", EmployeeWageComputation.daily_wage())
 else:
     print("Employee is absent")
-
-print("Daily Wage: ", EmployeeWageComputation.daily_wage())
